872bae4d0e49384bc12aa0be5aeb1894c1794f3c
Joseph Hansen <Joseph.Hansen@jhuapl.edu>
2020-10-13 03:07:44 -0400

    did the bonus objectives

--- mod1-solution/app.js
+++ mod1-solution/app.js
@@ -7,19 +7,24 @@ angular.module('LunchCheck', [])
 LunchCheckCntl.$inject = ['$scope','$filter'];
 function LunchCheckCntl($scope, $filter) {
   $scope.doIt = function() {
+    resetColors();
     var inputStr = $scope.theList;
     // First, check that input is defined
     if (typeof inputStr == "undefined" || inputStr == "") {
-        $scope.msgPlaceholder = "Please enter data first";
-        return;
+      $scope.msgPlaceholder = "Please enter data first";
+      $scope.msgClasses = "alert-danger";
+      $scope.inputClasses = "alert-danger";
+      return;
     }
     // Shows a message based on the number of items input
     var numItems = countValidItems(inputStr);
     if (numItems <= 3) {
-        $scope.msgPlaceholder = "Enjoy!";
+      $scope.msgPlaceholder = "Enjoy!";
     } else {
-        $scope.msgPlaceholder = "Too much!";
+      $scope.msgPlaceholder = "Too much!";
     }
+    $scope.msgClasses = "alert-success";
+    $scope.inputClasses = "alert-success";
     console.log(numItems);
   };
   var countValidItems = function(theInput) {
@@ -29,6 +34,13 @@ function LunchCheckCntl($scope, $filter) {
     console.log(s1);
     console.log(s2);
     return s2.length;
+  };
+  var resetColors = function() {
+    $scope.msgClasses = "";
+    $scope.inputClasses = "";
+  };
+  $scope.focusInput = function() {
+    $scope.inputClasses = "";
   };
 }
 })();
