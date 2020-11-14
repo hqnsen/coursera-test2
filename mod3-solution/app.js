9929b477b3c1b47fec8794011d230b58106cba1f
Joseph Hansen <Joseph.Hansen@jhuapl.edu>
2020-11-13 21:29:13 -0500

    working project

---
+++ mod3-solution/app.js
@@ -0,0 +1,93 @@
+(function () {
+  'use strict';
+  
+  
+  // Declaring my angular module here
+  angular.module('NarrowItDownApp', [])
+  .controller('NarrowItDownController', NarrowItDownController)
+  .service('MenuSearchService', MenuSearchService)
+  .directive('foundItems', FoundItemsDirective)
+  .constant('MenuItemsUrl', "https://davids-restaurant.herokuapp.com/menu_items.json");
+
+  function FoundItemsDirective() {
+    var ddo = {
+      restrict: "E",
+      templateUrl: 'foundItems.html',
+      scope: {
+        foundItems: '<',
+        onRemove: '&'
+      },
+      controller: FoundItemsDirectiveController,
+      controllerAs: 'list',
+      bindToController: true
+    };
+  
+    return ddo;
+  }
+
+  function FoundItemsDirectiveController() {
+    var ctrl = this;
+    ctrl.noResults = function() {
+      if (ctrl.foundItems === undefined) {
+        return true;
+      }
+      if (ctrl.foundItems.length < 1) {
+        return true;
+      }
+      return false;
+    };
+  }
+
+  NarrowItDownController.$inject = ['MenuSearchService'];
+  function NarrowItDownController(MenuSearchService) {
+    var ctrl = this;
+    ctrl.removeItem = function (itemIndex) {
+      if (ctrl.found !== undefined) {
+        ctrl.found.splice(itemIndex, 1);
+      } else {
+        console.log("Could not remove item at index "+itemIndex+" because list is empty");
+      }
+    };
+    ctrl.narrowIt = function () {
+      console.log("called narrowIt");
+      var menuMatchesPromise = MenuSearchService.getMatchedMenuItems(ctrl.searchTerm);
+      console.log("returned from getMatchedMenuItems");
+      menuMatchesPromise.then(function (response) {
+        console.log("getMatchedMenuItems has result:");
+        console.log(response);
+        ctrl.found = response;
+      }).catch(function (error) {
+        console.log(error);
+      });
+    };
+  }
+
+  MenuSearchService.$inject = ['$http', 'MenuItemsUrl'];
+  function MenuSearchService($http, MenuItemsUrl) {
+    var service = this;
+    service.getMatchedMenuItems = function(searchTerm) {
+      return $http({
+        method: "GET",
+        url: MenuItemsUrl
+      }).then(function(result) {
+        console.log("http has result:");
+        console.log(result);
+        // process results and only keep items that match
+        var foundItems = [];
+        for (var i = 0; i < result.data.menu_items.length; i++) {
+          var description = result.data.menu_items[i].description;
+          if (description.toLowerCase().indexOf(searchTerm.toLowerCase()) !== -1) {
+            //console.log(description);
+            foundItems.push(result.data.menu_items[i]);
+          }
+        }
+        return foundItems;
+      });
+    };
+  }
+
+
+  
+  
+})();
+  
